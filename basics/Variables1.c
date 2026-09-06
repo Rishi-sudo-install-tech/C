9d1973898e6561638162b8893e18b7523a7a9e60
Rishi <rishiproy@gmail.com>
2026-09-06 23:58:21 +0530

    Variables

---
+++ basics/Variables1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+int Num = 18;
+char YO = 'A';
+printf("%d\n" , Num);
+printf("%c\n" , YO); 
+
+printf("My age is %d and My Class is %C\n\n" , Num , YO);
+
+//Can be used for Automation.
+
+//Adding
+int x = 10;
+int y = 5;
+
+int sum = x + y;
+printf("The Sum is %d" , sum );
+return 0;
+}
---
+++ basics/Variables2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+double Radius = 15;
+double Pie = 3.141592653589793238;
+//Assumed Information for Basic Program
+
+double Area = Radius * Radius * Pie;
+//Formulla
+printf("Area of the circle is %lf sq cm." , Area);
+
+    return 0;
+
+
+}
+//Reusults
